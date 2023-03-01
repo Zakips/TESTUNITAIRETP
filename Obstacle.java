e41b4983f901556beef20c0513de87273e492ba2
Zakips <104654885+Zakips@users.noreply.github.com>
2023-03-01 16:15:59 +0100

    Add files via upload

---
+++ Obstacle.java
@@ -0,0 +1,18 @@
+public class Obstacle {
+
+    private final int posX;
+    private final int posY;
+
+    public Obstacle(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+}
